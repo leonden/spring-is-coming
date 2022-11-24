2638cf4810e41beb61cbdf2219fcff02c6b2dc5b
hocl1 <leon.hochwimmer@coop.ch>
2022-11-24 16:20:47 +0100

    minor changes

--- weather-app/src/main/resources/static/js/main.js
+++ weather-app/src/main/resources/static/js/main.js
@@ -11,9 +11,9 @@ window.addEventListener("DOMContentLoaded", (e) => {
 });
 
 function callGeocoder(searchInputValue) {
-  const xhttp = new XMLHttpRequest();
+  const ajaxRequest = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function () {
+  ajaxRequest.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let responseJson = JSON.parse(this.responseText);
       for (let item in responseJson) {
@@ -21,10 +21,27 @@ function callGeocoder(searchInputValue) {
       }
     }
   };
-  xhttp.open(
+  ajaxRequest.open(
     "GET",
     `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputValue}&limit=3&appid=1688017c7157a368a1d6a854d3a9ce02`,
     true
   );
-  xhttp.send();
+  ajaxRequest.send();
 }
+
+// function createMatches(matchTag, matchText, parentTag, className) {
+//   // Creates object with a tag name
+//   var tag = document.createElement(matchTag);
+//   // Creates the text (content)
+//   var text = document.createTextNode(matchText);
+//   // Creates the attribute
+//   var att = document.createAttribute("class");
+//   att.value = className;
+//   // Adds content to the tag
+//   tag.appendChild(text);
+//   // Adds attribute to the tag
+//   tag.setAttributeNode(att);
+//   // Adds tag to the parentTag
+//   var element = document.getElementsByTagName(parentTag)[0];
+//   element.appendChild(tag);
+// }
