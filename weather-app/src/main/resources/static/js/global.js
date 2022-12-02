d0fe35cce5794f9e431cc038b4f4575e80cd3ddc
hocl1 <leon.hochwimmer@coop.ch>
2022-12-02 07:59:36 +0100

    start background customization

--- weather-app/src/main/resources/static/js/global.js
+++ weather-app/src/main/resources/static/js/global.js
@@ -13,10 +13,8 @@ function callTimeZoneDatabase(latitude, longitude) {
   ajaxRequest.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       let responseJson = JSON.parse(this.responseText);
-      const localDate = responseJson.formatted.substr(0, 10);
-      const localTime = responseJson.formatted.substr(11, 18);
 
-      handleTime(localDate, localTime);
+      handleTime(responseJson);
     }
   };
   ajaxRequest.open(
@@ -27,7 +25,50 @@ function callTimeZoneDatabase(latitude, longitude) {
   ajaxRequest.send();
 }
 
-function handleTime(localDate, localTime) {
-  const localTimeToCalc = localTime.replaceAll(":", "");
-  console.log(parseInt(localTimeToCalc));
+function handleTime(responseJson) {
+  // const localDate = responseJson.formatted.substr(0, 10);
+  const localTime = responseJson.formatted.substr(11, 18).replaceAll(":", "");
+  const localTimeFormatted = parseInt(localTime);
+
+  console.log(localTimeFormatted);
+
+  setBackground(localTimeFormatted);
+}
+
+function setBackground(localTimeFormatted) {
+  const appContainer = document.querySelector(".app-container");
+
+  // set background sunrise
+  switch (localTimeFormatted) {
+    case localTimeFormatted >= 080000 && localTimeFormatted < 110000:
+      appContainer.style.background =
+        "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
+      break;
+    case localTimeFormatted >= 110000 && localTimeFormatted < 150000:
+      appContainer.style.background =
+        "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
+      break;
+    case localTimeFormatted >= 150000 && localTimeFormatted < 180000:
+      appContainer.style.background =
+        "linear-gradient(0deg, #ffdac2 0%, #fc7a41 50%, #ff6100 100%)";
+      break;
+    case localTimeFormatted >= 180000 && localTimeFormatted < 210000:
+      appContainer.style.background =
+        "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
+      break;
+    case localTimeFormatted >= 210000 && localTimeFormatted < 080000:
+      appContainer.style.background =
+        "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
+      break;
+    // default:
+    //   appContainer.style.background =
+    //     "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
+    //   break;
+  }
+
+  // set background sunrise
+
+  // set background sunrise
+
+  // set background sunrise
 }
