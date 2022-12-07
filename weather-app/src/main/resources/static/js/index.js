3d42fa777a7bae9539ac053e3af44be6600b5166
hocl1 <leon.hochwimmer@coop.ch>
2022-12-07 15:07:48 +0100

    ajax to fetch - index.js

--- weather-app/src/main/resources/static/js/index.js
+++ weather-app/src/main/resources/static/js/index.js
@@ -1,9 +1,9 @@
 const WEATHER_URL = "/src/main/resources/templates/weather.html";
 
-window.addEventListener("DOMContentLoaded", (e) => {
+window.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.querySelector(".search");
 
-  searchInput.addEventListener("keyup", (e) => {
+  searchInput.addEventListener("keyup", () => {
     let searchInputValue = searchInput.value;
     document.getElementById("matches").innerHTML = "";
     searchInput.style.borderRadius = "40px";
@@ -15,20 +15,23 @@ window.addEventListener("DOMContentLoaded", (e) => {
 });
 
 function callGeocoder(searchInputValue) {
-  const ajaxRequest = new XMLHttpRequest();
+  const searchInput = document.querySelector(".search");
 
-  ajaxRequest.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const searchInput = document.querySelector(".search");
-      searchInput.style.borderRadius = "20px 20px 0 0";
-
-      let responseJson = JSON.parse(this.responseText);
-      document.getElementById("matches").innerHTML = "";
-
-      if (responseJson.length != 0) {
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputValue}&limit=3&appid=1688017c7157a368a1d6a854d3a9ce02`
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error("error calling geocoder api");
+      }
+    })
+    .then((data) => {
+      if (data.length != 0) {
         searchInput.style.borderRadius = "20px 20px 0 0";
 
-        responseJson.forEach((locationItem) => {
+        data.forEach((locationItem) => {
           createMatches(
             locationItem.name,
             locationItem.state,
@@ -40,16 +43,7 @@ function callGeocoder(searchInputValue) {
       } else {
         searchInput.style.borderRadius = "40px";
       }
-    } else {
-      console.error("error calling geocoder api");
-    }
-  };
-  ajaxRequest.open(
-    "GET",
-    `http://api.openweathermap.org/geo/1.0/direct?q=${searchInputValue}&limit=3&appid=1688017c7157a368a1d6a854d3a9ce02`,
-    true
-  );
-  ajaxRequest.send();
+    });
 }
 
 function createMatches(matchText, state, country, latitude, longitude) {
