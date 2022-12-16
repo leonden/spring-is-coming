9a4bcc5bd1984cb3287d5e52ed0ee7a351a7bc4a
hocl1 <leon.hochwimmer@coop.ch>
2022-12-16 09:29:03 +0100

    forecast handle data / handle dom

--- weather-app/src/main/resources/static/js/forecast.js
+++ weather-app/src/main/resources/static/js/forecast.js
@@ -51,6 +51,90 @@ function handleDom(city, data) {
     forecastDayArray[i].innerHTML =
       Math.round(forecastJson[i].main.temp) + "&#176;C";
   }
+
+  handleImages(responseArray);
+}
+
+function handleImages(responseArray) {
+  const dayOne = document.querySelector("#data-forecast-img-1");
+  const dayTwo = document.querySelector("#data-forecast-img-2");
+  const dayThree = document.querySelector("#data-forecast-img-3");
+  const dayFour = document.querySelector("#data-forecast-img-4");
+  const dayFive = document.querySelector("#data-forecast-img-5");
+
+  const forecastDayArray = [dayOne, dayTwo, dayThree, dayFour, dayFive];
+
+  const forecastItems = [
+    {
+      desc: "Sun",
+      imgPath: "../static/assets/sun.png",
+    },
+    {
+      desc: "Clouds",
+      imgPath: "../static/assets/cloudy.png",
+    },
+    {
+      desc: "Snow",
+      imgPath: "../static/assets/snow.png",
+    },
+    {
+      desc: "Rain",
+      imgPath: "../static/assets/rain.png",
+    },
+    {
+      desc: "Extreme",
+      imgPath: "../static/assets/storm.png",
+    },
+  ];
+
+  let weatherDescription = [];
+
+  for (let i = 0; i < forecastDayArray.length; i++) {
+    getWeatherDescription(responseArray[i], weatherDescription);
+  }
+
+  console.log(weatherDescription);
+
+  appendImage(forecastDayArray, weatherDescription, forecastItems);
+}
+
+function appendImage(forecastDayArray, weatherDescription, forecastItems) {
+  for (let i = 0; i < forecastDayArray.length; i++) {
+    if (weatherDescription[i] === forecastItems[i].desc) {
+      console.log(forecastItems[i].imgPath);
+    }
+
+    switch (weatherDescription[i]) {
+      case forecastItems[0].desc:
+        console.log(forecastItems[0].imgPath);
+        break;
+      case forecastItems[1].desc:
+        console.log(forecastItems[1].imgPath);
+        break;
+      case forecastItems[2].desc:
+        console.log(forecastItems[2].imgPath);
+        break;
+      case forecastItems[3].desc:
+        console.log(forecastItems[3].imgPath);
+        break;
+      case forecastItems[4].desc:
+        console.log(forecastItems[4].imgPath);
+        break;
+    }
+
+    // let href = document.createAttribute("href");
+    // href.value = url;
+    // tag.appendChild(text);
+    // tag.setAttributeNode(href);
+    // let element = document.getElementById("matches");
+    // element.appendChild(tag);
+  }
+}
+
+function getWeatherDescription(responseArray, weatherDescription) {
+  let responseItem = responseArray;
+
+  weatherDescription.push(responseItem.weather[0].main);
 }
 
 function handleForecastResponse(responseArray) {
