2a42a7983ea66448038b017e6b4544874acb279b
hocl1 <leon.hochwimmer@coop.ch>
2022-12-22 15:31:46 +0100

    setup forecast api

--- weather-app/src/main/java/ch/coop/hocl1/weatherapp/models/openweather/ForecastModel.java
+++ weather-app/src/main/java/ch/coop/hocl1/weatherapp/models/openweather/ForecastModel.java
@@ -1,5 +1,6 @@
 package ch.coop.hocl1.weatherapp.models.openweather;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.time.LocalDateTime;
 @Setter
 public class ForecastModel {
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime date;
     private String description;
     private double temperature;
