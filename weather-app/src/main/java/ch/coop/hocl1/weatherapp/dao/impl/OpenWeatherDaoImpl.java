48cb41ce50cccc48a18500031b4a4a097076bdd9
hocl1 <leon.hochwimmer@coop.ch>
2022-12-23 07:26:40 +0100

    simplified

--- weather-app/src/main/java/ch/coop/hocl1/weatherapp/dao/impl/OpenWeatherDaoImpl.java
+++ weather-app/src/main/java/ch/coop/hocl1/weatherapp/dao/impl/OpenWeatherDaoImpl.java
@@ -1,7 +1,9 @@
 package ch.coop.hocl1.weatherapp.dao.impl;
 
 import ch.coop.hocl1.weatherapp.dao.OpenWeatherDao;
+import ch.coop.hocl1.weatherapp.factory.CurrentWeatherModelFactory;
 import ch.coop.hocl1.weatherapp.factory.ForecastModelListFactory;
+import ch.coop.hocl1.weatherapp.models.openweather.CurrentWeatherModel;
 import ch.coop.hocl1.weatherapp.models.openweather.ForecastModel;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -53,18 +55,34 @@ public class OpenWeatherDaoImpl implements OpenWeatherDao {
 
     @Override
     public List<ForecastModel> readForecast(double latitude, double longitude) {
-
-        MultiValueMap<String, String> queryParameters = new LinkedMultiValueMap<>();
-        queryParameters.put("appid", Collections.singletonList(apiKey));
-        queryParameters.put("units", Collections.singletonList("metric"));
-        queryParameters.put("lat", Collections.singletonList(String.valueOf(latitude)));
-        queryParameters.put("lon", Collections.singletonList(String.valueOf(longitude)));
+        MultiValueMap<String, String> queryParameters = buildQueryParameters(latitude, longitude);
 
         String responseAsString = webClient.get()
                 .uri(uriBuilder -> uriBuilder.path("/forecast").queryParams(queryParameters).build())
                 .retrieve().bodyToMono(String.class).block();
 
         return ForecastModelListFactory.create(responseAsString);
+    }
+
+    @Override
+    public CurrentWeatherModel readCurrentWeather(double latitude, double longitude) {
+        MultiValueMap<String, String> queryParameters = buildQueryParameters(latitude, longitude);
+
+        String responseAsString = webClient.get()
+                .uri(uriBuilder -> uriBuilder.path("/weather").queryParams(queryParameters).build())
+                .retrieve().bodyToMono(String.class).block();
+
+        return CurrentWeatherModelFactory.create(responseAsString);
+    }
+
+    private MultiValueMap<String, String> buildQueryParameters(double latitude, double longitude) {
+        MultiValueMap<String, String> queryParameters = new LinkedMultiValueMap<>();
+        queryParameters.put("appid", Collections.singletonList(apiKey));
+        queryParameters.put("units", Collections.singletonList("metric"));
+        queryParameters.put("lat", Collections.singletonList(String.valueOf(latitude)));
+        queryParameters.put("lon", Collections.singletonList(String.valueOf(longitude)));
+
+        return queryParameters;
     }
 
 }
