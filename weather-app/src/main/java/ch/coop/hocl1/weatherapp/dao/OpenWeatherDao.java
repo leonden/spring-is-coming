ac1b0c255756c6fa6dca94954ee81cfd036be3ed
hocl1 <leon.hochwimmer@coop.ch>
2022-12-23 07:25:02 +0100

    add current weather

--- weather-app/src/main/java/ch/coop/hocl1/weatherapp/dao/OpenWeatherDao.java
+++ weather-app/src/main/java/ch/coop/hocl1/weatherapp/dao/OpenWeatherDao.java
@@ -1,5 +1,6 @@
 package ch.coop.hocl1.weatherapp.dao;
 
+import ch.coop.hocl1.weatherapp.models.openweather.CurrentWeatherModel;
 import ch.coop.hocl1.weatherapp.models.openweather.ForecastModel;
 
 import java.time.LocalDateTime;
@@ -8,5 +9,7 @@ import java.util.List;
 public interface OpenWeatherDao {
 
     List<ForecastModel> readForecast(double latitude, double longitude);
+
+    CurrentWeatherModel readCurrentWeather(double latitude, double longitude);
 
 }
