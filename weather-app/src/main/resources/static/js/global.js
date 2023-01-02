71d09322b76b5097d5779dd3422f2855be445df5
hocl1 <leon.hochwimmer@coop.ch>
2023-01-02 15:21:56 +0100

    minor adjustments

--- weather-app/src/main/resources/static/js/global.js
+++ weather-app/src/main/resources/static/js/global.js
@@ -40,55 +40,47 @@ function handleTime(data) {
 function setBackground(localTimeFormatted) {
   const appContainer = document.querySelector(".app-container");
 
-  if (localTimeFormatted >= 060000 && localTimeFormatted < 900000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #ffe5c1 0%, #ffb64f 50%, #ff7c1f 100%)";
-  }
-  if (localTimeFormatted >= 090000 && localTimeFormatted < 100000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  }
-  if (localTimeFormatted >= 100000 && localTimeFormatted < 140000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #b9efff 0%, #3dbce3 50%, #0060cd 100%)";
-  }
-  if (localTimeFormatted >= 140000 && localTimeFormatted < 180000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #ffea82 0%, #ffbd39 50%, #ff7722 100%)";
-  }
-  if (localTimeFormatted >= 180000 && localTimeFormatted < 210000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #c3beff 0%, #5f80ec 50%, #0c3dae 100%)";
-  }
-  if (localTimeFormatted >= 210000 && localTimeFormatted < 070000) {
-    appContainer.style.background =
-      "linear-gradient(0deg, #6d4cc1 0%, #341ea7 50%, #1b005f 100%)";
-  }
+  const sunrise = [
+    localTimeFormatted >= 060000 && localTimeFormatted < 900000,
+    "linear-gradient(0deg, #ffe5c1 0%, #ffb64f 50%, #ff7c1f 100%)",
+  ];
+  const morning = [
+    localTimeFormatted >= 090000 && localTimeFormatted < 100000,
+    "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)",
+  ];
+  const lunch = [
+    localTimeFormatted >= 100000 && localTimeFormatted < 140000,
+    "linear-gradient(0deg, #b9efff 0%, #3dbce3 50%, #0060cd 100%)",
+  ];
+  const afternoon = [
+    localTimeFormatted >= 140000 && localTimeFormatted < 180000,
+    "linear-gradient(0deg, #ffea82 0%, #ffbd39 50%, #ff7722 100%)",
+  ];
+  const evening = [
+    localTimeFormatted >= 180000 && localTimeFormatted < 210000,
+    "linear-gradient(0deg, #c3beff 0%, #5f80ec 50%, #0c3dae 100%)",
+  ];
+  const night = [
+    localTimeFormatted >= 210000 && localTimeFormatted < 070000,
+    "linear-gradient(0deg, #6d4cc1 0%, #341ea7 50%, #1b005f 100%)",
+  ];
 
-  // switch (localTimeFormatted) {
-  //   case localTimeFormatted >= 080000 && localTimeFormatted < 110000:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  //     break;
-  //   case localTimeFormatted >= 110000 && localTimeFormatted < 150000:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  //     break;
-  //   case localTimeFormatted >= 150000 && localTimeFormatted < 180000:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #ffdac2 0%, #fc7a41 50%, #ff6100 100%)";
-  //     break;
-  //   case localTimeFormatted >= 180000 && localTimeFormatted < 210000:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  //     break;
-  //   case localTimeFormatted >= 210000 && localTimeFormatted < 080000:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  //     break;
-  //   default:
-  //     appContainer.style.background =
-  //       "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)";
-  //     break;
-  // }
+  if (sunrise[0]) {
+    appContainer.style.background = sunrise[1];
+  }
+  if (morning[0]) {
+    appContainer.style.background = morning[1];
+  }
+  if (lunch[0]) {
+    appContainer.style.background = lunch[1];
+  }
+  if (afternoon[0]) {
+    appContainer.style.background = afternoon[1];
+  }
+  if (evening[0]) {
+    appContainer.style.background = evening[1];
+  }
+  if (night[0]) {
+    appContainer.style.background = night[1];
+  }
 }
