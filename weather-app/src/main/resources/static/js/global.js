f64f12144d29cb35a4165473f93810569d1cbe83
hocl1 <leon.hochwimmer@coop.ch>
2023-01-05 15:55:13 +0100

    fixed time

--- weather-app/src/main/resources/static/js/global.js
+++ weather-app/src/main/resources/static/js/global.js
@@ -28,59 +28,63 @@ function callTimeZoneDatabase(latitude, longitude) {
 // -------------------------------------------------------------------
 
 function handleTime(data) {
-  // const localDate = data.formatted.substr(0, 10);
   const localTime = data.formatted.substr(11, 18).replaceAll(":", "");
   const localTimeFormatted = parseInt(localTime);
+  const localHour = parseInt(retrieveLocalHour(localTimeFormatted));
 
-  setBackground(localTimeFormatted);
+  setBackground(localHour);
 }
 
 // -------------------------------------------------------------------
 
-function setBackground(localTimeFormatted) {
+function retrieveLocalHour(localTimeFormatted) {
+  const time = localTimeFormatted;
+
+  if (time.toString().length === 5) {
+    return time.toString().substr(0, 1);
+  } else if (time.toString().length === 6) {
+    return time.toString().substr(0, 2);
+  }
+}
+
+// -------------------------------------------------------------------
+
+function setBackground(localHour) {
   const appContainer = document.querySelector(".app-container");
 
-  const sunrise = [
-    localTimeFormatted >= 060000 && localTimeFormatted < 900000,
+  // order: sunrise, morning, lunch, afternoon, evening, night, night
+  const dayTime = [
+    localHour >= 7 && localHour < 9,
+    localHour >= 9 && localHour < 10,
+    localHour >= 10 && localHour < 14,
+    localHour >= 14 && localHour < 18,
+    localHour >= 18 && localHour < 21,
+    localHour >= 21 && localHour <= 24,
+    localHour >= 0 && localHour < 7,
     "linear-gradient(0deg, #ffe5c1 0%, #ffb64f 50%, #ff7c1f 100%)",
-  ];
-  const morning = [
-    localTimeFormatted >= 090000 && localTimeFormatted < 100000,
     "linear-gradient(0deg, #eeedff 0%, #aec1ff 50%, #787aff 100%)",
-  ];
-  const lunch = [
-    localTimeFormatted >= 100000 && localTimeFormatted < 140000,
     "linear-gradient(0deg, #b9efff 0%, #3dbce3 50%, #0060cd 100%)",
-  ];
-  const afternoon = [
-    localTimeFormatted >= 140000 && localTimeFormatted < 180000,
     "linear-gradient(0deg, #ffea82 0%, #ffbd39 50%, #ff7722 100%)",
-  ];
-  const evening = [
-    localTimeFormatted >= 180000 && localTimeFormatted < 210000,
     "linear-gradient(0deg, #c3beff 0%, #5f80ec 50%, #0c3dae 100%)",
-  ];
-  const night = [
-    localTimeFormatted >= 210000 && localTimeFormatted < 070000,
     "linear-gradient(0deg, #6d4cc1 0%, #341ea7 50%, #1b005f 100%)",
   ];
 
-  if (sunrise[0]) {
-    appContainer.style.background = sunrise[1];
+  if (dayTime[0]) {
+    appContainer.style.background = dayTime[7];
   }
-  if (morning[0]) {
-    appContainer.style.background = morning[1];
+  if (dayTime[1]) {
+    appContainer.style.background = dayTime[8];
   }
-  if (lunch[0]) {
-    appContainer.style.background = lunch[1];
+  if (dayTime[2]) {
+    appContainer.style.background = dayTime[9];
   }
-  if (afternoon[0]) {
-    appContainer.style.background = afternoon[1];
+  if (dayTime[3]) {
+    appContainer.style.background = dayTime[10];
   }
-  if (evening[0]) {
-    appContainer.style.background = evening[1];
+  if (dayTime[4]) {
+    appContainer.style.background = dayTime[11];
   }
-  if (night[0]) {
-    appContainer.style.background = night[1];
+  if (dayTime[5] || dayTime[6]) {
+    appContainer.style.background = dayTime[12];
   }
 }
